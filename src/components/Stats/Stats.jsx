01360ab09f70bddcb7a882eb52455f5943f85217
HUMAYOU_ALI_SARAL <humayouali63@gmail.com>
2025-04-28 13:09:33 +0500

    vercel fixed

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -12,10 +12,10 @@ import { useActiveGiveaway } from "@/context/ActiveGiveaway";
 export default function Stats() {
   const { activeGiveaways, activeGiveaway, changeActiveGiveaway } = useActiveGiveaway();
   const stats = [
-    { title: "Giveaway Name", value: activeGiveaway.name },
-    { title: "Giveaway Id", value: activeGiveaway.id },
-    { title: "Total Participants", value: activeGiveaway.totalParticipants },
-    { title: "Winner Email Address", value: activeGiveaway.winner?activeGiveaway.winner:"Pending" },
+    { title: "Giveaway Name", value: activeGiveaway?.name },
+    { title: "Giveaway Id", value: activeGiveaway?.id },
+    { title: "Total Participants", value: activeGiveaway?.totalParticipants },
+    { title: "Winner Email Address", value: activeGiveaway?.winner?activeGiveaway?.winner:"Pending" },
   ];
 useEffect(()=>{
   console.log("this is active",activeGiveaway)
@@ -30,7 +30,7 @@ useEffect(()=>{
       </div> */}
       <StatCards stats={stats} />
       <div className="pt-[30px]">
-        <DataTable giveawayId={activeGiveaway.id.toString()} />
+        <DataTable giveawayId={activeGiveaway?.id.toString()} />
       </div>
     </div>
   );
